067806a78b77c90e25cfcff78d5740b0e5e24e52
aeloh1m <melowgz@gmail.com>
2024-08-08 18:43:30 -0300

    first commit

---
+++ src/runTest.bat
@@ -0,0 +1,15 @@
+@echo off
+rem Set the directory containing the .bat file as the working directory
+cd /d "%~dp0"
+
+rem Compile the source code
+gcc -I..\include -L..\lib -o main.exe main.c -lglfw3 -lgdi32 -lopengl32
+
+rem Check for compilation errors
+if %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed
+    exit /b %ERRORLEVEL%
+)
+
+rem Run the executable
+start "" "main.exe"
